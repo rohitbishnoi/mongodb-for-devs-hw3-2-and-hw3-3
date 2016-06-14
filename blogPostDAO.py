641c968fd3622860b9b5c6518005193b1bfa32b1
Rohit Kumar <rohit.kumar@intelligrape.com>
2016-06-14 12:38:45 +0530

    Solution for Homework 3.2 and 3.3

--- blogPostDAO.py
+++ blogPostDAO.py
@@ -54,6 +54,7 @@ class BlogPostDAO:
         # now insert the post
         try:
             # XXX HW 3.2 Work Here to insert the post
+            self.posts.insert(post)
             print "Inserting the post"
         except:
             print "Error inserting post"
@@ -67,7 +68,7 @@ class BlogPostDAO:
         cursor = iter(())  # Using an empty itable for a placeholder so blog compiles before you make your changes
 
         # XXX HW 3.2 Work here to get the posts
-
+        cursor = self.posts.find().sort([("date", -1)]).limit(num_posts)
         l = []
 
         for post in cursor:
@@ -90,7 +91,7 @@ class BlogPostDAO:
 
         post = None
         # XXX 3.2 Work here to retrieve the specified post
-
+        post = self.posts.find_one({"permalink": permalink})
         if post is not None:
             # fix up date
             post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p")
@@ -107,8 +108,8 @@ class BlogPostDAO:
 
         try:
             # XXX HW 3.3 Work here to add the comment to the designated post. When done, modify the line below to return the number of documents updated by your modification, rather than just -1.
-
-            return -1  # Change this to return the number of documents updated by the code for HW 3.3
+            noOfDocsUpdated = self.posts.update({"permalink": permalink}, {"$push" : {"comments" : comment}})
+            return noOfDocsUpdated  # Change this to return the number of documents updated by the code for HW 3.3
 
         except:
             print "Could not update the collection, error"
